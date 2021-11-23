8091e866350c8fa688efdb53b85680b36defffab
Myrvold <marte.myrvold@nordicsemi.no>
2021-11-23 15:41:45 +0100

    removed unnecessary files

--- light_switch_broadcast_custom_cmd/pca10056/blank/ses/Output/zigbee_light_switch_pca10056 Release/Obj/ses_startup_nrf52840.d
+++
@@ -1,2 +0,0 @@
-Output/zigbee_light_switch_pca10056\ Release/Obj/ses_startup_nrf52840.o: \
- C:\nRF5\ SDK\nRF5_SDK_T&Z_v4.1.0\modules\nrfx\mdk\ses_startup_nrf52840.s
--- light_switch_broadcast_custom_cmd/pca10056/blank/ses/Output/zigbee_light_switch_pca10056 Release/Obj/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/zigbee_light_switch_pca10056\ Release/Obj/thumb_crt0.o: \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52/source/thumb_crt0.s
